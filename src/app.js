46d9bf74f80df3be64e048485d55f0804594678a
Typee8 <wojciechjasiak1324@gmail.com>
2024-07-03 14:22:21 +0200

    Change btn--end to absolute position - checkpoint

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import TasksManager from "./components/TasksManager";
 
+import "./styles/reset.css";
 import "./styles/fonts.css";
 import "./styles/main.css";
 import "./styles/tablet.css";
